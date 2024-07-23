95970ab12d99ddc0259e4ec880d5be542c364a4c
Valeria-Ramirez130 <116677886+Valeria-Ramirez130@users.noreply.github.com>
2024-07-23 17:15:42 -0500

    se inicio page citas

--- src/pages/Citas/citas.jsx
+++ src/pages/Citas/citas.jsx
@@ -1,49 +1,210 @@
-import React from "react";
+import React, { useState } from "react";
 import "./citas.css";
 
 export default function Citas() {
-  console.log("HOLA DESDE CITAS");
+  const [formData, setFormData] = useState({
+    nombre: "",
+    telefono: "",
+    email: "",
+    nombreMascota: "",
+    especie: "",
+    motivo: "",
+    fecha: "",
+    hora: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Datos del formulario:", formData);
+    // Aquí puedes agregar la lógica para enviar los datos del formulario
+  };
+
   return (
-    <div className="citas">
-      citas
-      <table>
-        <tr>
-          <td>Nombre</td>
-          <td>Apellido</td>
-          <td>Telefono</td>
-          <td>Correo</td>
-          <td>Fecha</td>
-          <td>Hora</td>
-          <td>Descripcion</td>
-        </tr>
-        <tr>
-          <td>Nombre</td>
-          <td>Apellido</td>
-          <td>Telefono</td>
-          <td>Correo</td>
-          <td>Fecha</td>
-          <td>Hora</td>
-          <td>Descripcion</td>
-        </tr>
-        <tr>
-          <td>Nombre</td>
-          <td>Apellido</td>
-          <td>Telefono</td>
-          <td>Correo</td>
-          <td>Fecha</td>
-          <td>Hora</td>
-          <td>Descripcion</td>
-        </tr>
-        <tr>
-          <td>Nombre</td>
-          <td>Apellido</td>
-          <td>Telefono</td>
-          <td>Correo</td>
-          <td>Fecha</td>
-          <td>Hora</td>
-          <td>Descripcion</td>
-        </tr>
-      </table>
+    <div>
+      <div className="contenedor-formulario">
+        <h2 className="titulo-formulario">¡Agenda tu cita!</h2>
+        <form onSubmit={handleSubmit} className="formulario-citas">
+          {/* Campos del formulario */}
+          <label>
+            Nombre:
+            <input
+              type="text"
+              name="nombre"
+              value={formData.nombre}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Teléfono:
+            <input
+              type="tel"
+              name="telefono"
+              value={formData.telefono}
+              onChange={handleChange}
+              pattern="[0-9]{10}"
+              title="Por favor, ingresa un número de teléfono válido de 10 dígitos"
+              required
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Nombre mascota:
+            <input
+              type="text"
+              name="nombreMascota"
+              value={formData.nombreMascota}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Especie:
+            <select
+              name="especie"
+              value={formData.especie}
+              onChange={handleChange}
+              required
+            >
+              <option value="">Selecciona una especie</option>
+              <option value="perro">Perro</option>
+              <option value="gato">Gato</option>
+            </select>
+          </label>
+          <label>
+            Motivo de la cita:
+            <select
+              name="motivo"
+              value={formData.motivo}
+              onChange={handleChange}
+              required
+            >
+              <option value="">Selecciona un motivo</option>
+              <option value="bañoGrande">
+                ✓ Baño y peluquería perro raza grande
+              </option>
+              <option value="bañoPequeño">
+                ✓ Baño y peluquería perro raza pequeña
+              </option>
+              <option value="consulta">✓ Consulta veterinaria</option>
+              <option value="control">✓ Control médico</option>
+              <option value="muestras">✓ Toma muestras para laboratorio</option>
+              <option value="vacunacion">✓ Vacunación</option>
+              <option value="vacunacion">✓ Baño y peluquería gato</option>
+            </select>
+          </label>
+          <label>
+            Fecha:
+            <input
+              type="date"
+              name="fecha"
+              value={formData.fecha}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <label>
+            Hora:
+            <input
+              type="time"
+              name="hora"
+              value={formData.hora}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <button type="submit" className="button btn primary request-button">
+            SOLICITAR CITA
+          </button>
+        </form>
+      </div>
+
+      <div className="appointments-container">
+        <h5 className="text-bold">Citas más frecuentes</h5>
+        <div className="appointments-grid">
+          <div className="appointments-row">
+            <div className="appointment">
+              <h5>Consulta veterinaria</h5>
+              <button
+                type="button"
+                className="button btn primary request-button"
+              >
+                Solicitar
+              </button>
+            </div>
+            <div className="appointment">
+              <h5>Control médico</h5>
+              <button
+                type="button"
+                className="button btn primary request-button"
+              >
+                Solicitar
+              </button>
+            </div>
+            <div className="appointment">
+              <h5>Baño y peluquería perro raza grande</h5>
+              <button
+                type="button"
+                className="button btn primary request-button"
+              >
+                Solicitar
+              </button>
+            </div>
+            <div className="appointment">
+              <h5>Baño y peluquería perro raza pequeña</h5>
+              <button
+                type="button"
+                className="button btn primary request-button"
+              >
+                Solicitar
+              </button>
+            </div>
+          </div>
+          <div className="appointments-row">
+            <div className="appointment">
+              <h5>Toma muestras para laboratorio</h5>
+              <button
+                type="button"
+                className="button btn primary request-button"
+              >
+                Solicitar
+              </button>
+            </div>
+            <div className="appointment">
+              <h5>Baño y peluquería gato</h5>
+              <button
+                type="button"
+                className="button btn primary request-button"
+              >
+                Solicitar
+              </button>
+            </div>
+            <div className="appointment">
+              <h5>Vacunación</h5>
+              <button
+                type="button"
+                className="button btn primary request-button"
+              >
+                Solicitar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Inicio/inicio.jsx
+++ src/pages/Inicio/inicio.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import imgInicio from "../../img/imageninicio.jpg"; // Asegúrate de la ruta correcta
 import "./inicio.css"; // Importa el archivo de estilos
 
 function Inicio() {
+  const navigate = useNavigate(); // Hook para navegar entre rutas
+
+  const handleButtonClick = () => {
+    navigate("/citas"); // Navega a la ruta "/citas"
+  };
+
   return (
     <div className="inicio-container">
       <img src={imgInicio} alt="Imagen de Inicio" className="inicio-img" />
       <div className="inicio-content">
         <h1 className="inicio-title">¡Bienvenido!</h1>
-        <button className="inicio-button">Programar cita</button>
+        <button className="inicio-button" onClick={handleButtonClick}>
+          Programar cita
+        </button>
       </div>
     </div>
   );
